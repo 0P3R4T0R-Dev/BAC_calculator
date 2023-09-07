8406c362ba36edbf9115c2799d56d629ffd99181
Stefan de Jager <stefan.dj1356@gmail.com>
2023-09-07 19:37:48 +0200

    Starting Basic setup of info sharing and logic

--- Backend/index.js
+++ Backend/index.js
@@ -11,6 +11,41 @@ const io = require("socket.io")(server, {
 });
 server.listen(port, () => { console.log(`Server running at http://localhost:${port}`); });
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+let height = 0;
+let weight = 0;
+let age = 0;
+
+let drinks = [];
+
+io.on("connection", (socket) => {
+    console.log("A user connected with socket ID: " + socket.id);
+    socket.on("disconnect", () => { console.log("A user disconnected"); });
+    
+    socket.on("updateWHA", (data) => {
+        console.log("Received data from client: " + JSON.stringify(data));
+        height = data.height;
+        weight = data.weight;
+        age = data.age;
+    });
+
+    socket.on("Add_drink", (data) => {
+        console.log("Received drink from client: " + JSON.stringify(data));
+        drinks.push(data);
+
+        let drinkIterator = drinks.values();
+        for(let drink of drinkIterator) {
+            console.log("Drink: " + JSON.stringify(drink));
+        }
+    });
+
+
+});
+
+
+function calculateWidmarkFactor(heightM, weightKG, age) {
+    return .62544 + .13664 * heightM - weightKG * (.00189 + .002425 / (heightM * heightM)) + 1 / (weightKG * (.57986 + 2.54 * heightM - .02255 * age))
+}
+
+function calculateAlcoholGrams(milliliters, percent) {
+    return (milliliters/1000) * percent * .789;
+}
--- Frontend/script.js
+++ Frontend/script.js
@@ -1,5 +1,49 @@
-let button = document.getElementById('button');
+const SERVER_URL = window.location.host === "localhost:3000" ? "http://localhost:3000" : "https://my-website.com";
+let socket = io.connect(SERVER_URL);
 
-button.addEventListener('click', function() {
-    console.log('Button clicked');
+socket.on("connect", () => {
+    console.log("Connected to server");
 });
+
+
+
+let button_HWA = document.getElementById('button_HWA');
+
+button_HWA.addEventListener('click', function() {
+    let height = parseInt(document.getElementById('input_H').value);
+    let weight = parseInt(document.getElementById('input_W').value);
+    let age = parseInt(document.getElementById('input_A').value);
+    
+    if(isNaN(height) || isNaN(weight) || isNaN(age)) {
+        console.log("Invalid data");
+        return;
+    }
+    socket.emit('updateWHA', {height: height, weight: weight, age: age});
+    console.log("Sent data to server");
+});
+
+
+let halflife = 0;
+let milliliters = 0;
+let percent = 0;
+let time = 0;
+
+let button_drinks = document.getElementById('button_drinks');
+
+button_drinks.addEventListener('click', function() {
+    halflife = Number(document.querySelector('input[name="hunger_level"]:checked').value);
+    milliliters = parseInt(document.getElementById('input_ml').value);
+    percent = parseInt(document.getElementById('input_ABV').value);
+    time = new Date().getTime();
+    if(isNaN(halflife) || isNaN(milliliters) || isNaN(percent)) {
+        console.log("Invalid data");
+        return;
+    }
+    socket.emit("Add_drink", {halflife: halflife, milliliters: milliliters, percent: percent, time: time});
+    console.log("Sent drink to server");
+    document.getElementById('input_ml').value = "";
+    document.getElementById('input_ABV').value = "";
+});
+
+
+
